dbaabcab7a357b3853468f1667ecf81c0823f672
Igor Krivenko <igor.s.krivenko@gmail.com>
2013-07-31 18:19:38 +0200

    Use checked python -> C++ conversions in any_body_operator.pyx

--- python/many_body_operator.pyx
+++ python/many_body_operator.pyx
@@ -21,38 +21,38 @@ cdef class Operator:
     # Addition            
     def __iadd__(self, other):
         if isinstance(other,Operator):
-            self._c[0] += (<Operator> other)._c[0]
+            self._c[0] += (<Operator?> other)._c[0]
         else:
-            self._c[0] += <double> other
+            self._c[0] += <double?> other
         return self
         
     def __add__(A, B):
         O = Operator()
         for x in (A,B):
             if isinstance(x,Operator):
-                (<Operator> O)._c[0] += (<Operator> x)._c[0]
+                (<Operator?> O)._c[0] += (<Operator?> x)._c[0]
             else:
-                (<Operator> O)._c[0] += <double> x
+                (<Operator?> O)._c[0] += <double?> x
         return O
     
     # Subtraction
     def __isub__(self, other):
         if isinstance(other,Operator):
-            self._c[0] -= (<Operator> other)._c[0]
+            self._c[0] -= (<Operator?> other)._c[0]
         else:
-            self._c[0] -= <double> other
+            self._c[0] -= <double?> other
         return self
         
     def __sub__(A, B):
         O = Operator()
         if isinstance(A,Operator):
-            (<Operator> O)._c[0] += (<Operator> A)._c[0]
+            (<Operator?> O)._c[0] += (<Operator?> A)._c[0]
         else:
-            (<Operator> O)._c[0] += <double> A
+            (<Operator?> O)._c[0] += <double?> A
         if isinstance(B,Operator):
-            (<Operator> O)._c[0] -= (<Operator> B)._c[0]
+            (<Operator?> O)._c[0] -= (<Operator?> B)._c[0]
         else:
-            (<Operator> O)._c[0] -= <double> B
+            (<Operator?> O)._c[0] -= <double?> B
         return O
     
     def __neg__(self):
@@ -63,20 +63,20 @@ cdef class Operator:
     # Multiplication
     def __imul__(self, other):
         if isinstance(other,Operator):
-            self._c[0] *= (<Operator> other)._c[0]
+            self._c[0] *= (<Operator?> other)._c[0]
         else:
-            self._c[0] *= <double> other
+            self._c[0] *= <double?> other
         return self
         
     def __mul__(A, B):
         O = Operator()
         if isinstance(A,Operator):
             if isinstance(B,Operator):
-                (<Operator> O)._c[0] = (<Operator> A)._c[0] * (<Operator> B)._c[0]
+                (<Operator?> O)._c[0] = (<Operator?> A)._c[0] * (<Operator?> B)._c[0]
             else:
-                (<Operator> O)._c[0] = (<Operator> A)._c[0] * (<double> B)
+                (<Operator?> O)._c[0] = (<Operator?> A)._c[0] * (<double?> B)
         else:
-            (<Operator> O)._c[0] = (<Operator> B)._c[0] * (<double> A)
+            (<Operator?> O)._c[0] = (<Operator?> B)._c[0] * (<double?> A)
         return O
         
 def C(block,index):

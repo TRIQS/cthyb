a4d6532ad25a02adc563259073156fe18ee4fdb8
Igor Krivenko <igor.s.krivenko@gmail.com>
2013-09-19 18:58:39 +0200

    Made visual output of plot.krylov.stats.py more obvious.

--- test/c++/plot.krylov.stats.py
+++ test/c++/plot.krylov.stats.py
@@ -2,37 +2,35 @@
 
 stats_filename = "dims.stats.dat"
 
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
 stats = {}
-
-x = []
-y = []
+space_dims = []
 for line in open(stats_filename,'r'):
 	if line[0] == '#': continue
 	space_dim, krylov_dim, count = map(lambda s: int(s), line.split('\t'))
-	stats[(space_dim,krylov_dim)] = count
 	
-	x.append(space_dim)
-	y.append(krylov_dim)
+	if not space_dim in space_dims:
+		space_dims.append(space_dim)
+		stats[space_dim] = {}
+	stats[space_dim][krylov_dim] = count
 
-x = np.arange(min(x),max(x)+2)
-y = np.arange(min(y),max(y)+2)
-z = np.zeros((len(x)-1,len(y)-1),dtype=int)
+space_dims.sort()
 
-for nm in stats:
-    xi = nm[0] - x[0]
-    yi = nm[1] - y[0]
-    z[xi,yi] = stats[nm]
+fig = plt.gcf()
+ax = fig.add_subplot(111, projection='3d')
 
-plt.pcolor(x,y,z.T,cmap="gray")
-#plt.imshow(z,extent=[x[0],x[-1],y[0],y[-1]])
+for n, space_dim in enumerate(space_dims):
+	counts = np.zeros(space_dim,dtype=int)
+	s = stats[space_dim]
+	for krylov_dim in s: counts[krylov_dim-1] = s[krylov_dim]
+	ax.bar(np.arange(0,space_dim),counts,zs=n,zdir='x')
 
-ax = plt.gca()
-ax.set_xlim(x[0],x[-1])
-ax.set_ylim(y[0],y[-1])
+ax.set_xticklabels(space_dims)
 ax.set_xlabel("Space Dim")
 ax.set_ylabel("Krylov Dim")
+ax.set_zlabel("Count")
 
 plt.show()

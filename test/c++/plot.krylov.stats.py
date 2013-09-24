c85cc94f546ccaa80818e74036eb78b13b09915b
Igor Krivenko <igor.s.krivenko@gmail.com>
2013-09-24 17:47:54 +0200

    Average Krylov dimension is now calculated in plot.krylov.stats.py

--- test/c++/plot.krylov.stats.py
+++ test/c++/plot.krylov.stats.py
@@ -8,6 +8,8 @@ import numpy as np
 
 stats = {}
 space_dims = []
+total_counts = 0
+average_krylov_dim = 0
 for line in open(stats_filename,'r'):
 	if line[0] == '#': continue
 	space_dim, krylov_dim, count = map(lambda s: int(s), line.split('\t'))
@@ -16,6 +18,9 @@ for line in open(stats_filename,'r'):
 		space_dims.append(space_dim)
 		stats[space_dim] = {}
 	stats[space_dim][krylov_dim] = count
+	
+	average_krylov_dim += krylov_dim*count
+	total_counts += count
 
 space_dims.sort()
 
@@ -26,11 +31,13 @@ for n, space_dim in enumerate(space_dims):
 	counts = np.zeros(space_dim,dtype=int)
 	s = stats[space_dim]
 	for krylov_dim in s: counts[krylov_dim-1] = s[krylov_dim]
-	ax.bar(np.arange(0,space_dim),counts,zs=n,zdir='x')
+	ax.bar(np.arange(0,space_dim),counts,zs=0.1*n,zdir='x')
 
 ax.set_xticklabels(space_dims)
 ax.set_xlabel("Space Dim")
 ax.set_ylabel("Krylov Dim")
 ax.set_zlabel("Count")
+
+print "Average Krylov dimension:", float(average_krylov_dim)/total_counts
 
 plt.show()

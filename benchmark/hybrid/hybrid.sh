73486156f890a75507825724509e288bc89c510e
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-01-25 22:37:33 +0100

    'hybrid' benchmark is adapted for the new code.

--- benchmark/hybrid/hybrid.sh
+++ benchmark/hybrid/hybrid.sh
@@ -1,16 +1,20 @@
 #!/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT="./5_plus_5.py"
-PARAMS="./params.py"
+SCRIPT="../5+5/5_plus_5.py"
+PARAMS="params.py"
+REGEXP="\[Node 0\] Simulation lasted: \([0-9]\+\) seconds"
 
-cp "${PARAMS}" "${PARAMS}.bak"
+echo "Hybrid (matrix/Krylov) regime benchmarking script."
+echo "See small_matrix_size.*.log for resulting execution times."
+
 for small_matrix_size in 0 1 5 10 25 50 100
 do
-    sed -i "s/^\(p\[\"krylov_small_matrix_size\"\] =\)[ 0-9]\+/\1 ${small_matrix_size}/" ${PARAMS}
-    sed -i "s/^\(results_file_name =\).*/\1 \"small_matrix_size.${small_matrix_size}.h5\"/" ${PARAMS}
+    cp "${PARAMS}.in" "${PARAMS}"
+    echo "p[\"krylov_small_matrix_size\"] = ${small_matrix_size}" >> ${PARAMS}
+    echo "results_file_name = \"small_matrix_size.${small_matrix_size}.h5\"" >> ${PARAMS}
     for run in `seq 0 5`; do
-        ${SCRIPT} | grep "Simulation lasted:" >> "small_matrix_size.${small_matrix_size}.log"
+        ${SCRIPT} | grep "${REGEXP}" | sed -e "s/${REGEXP}/\1/" >> "small_matrix_size.${small_matrix_size}.log"
     done
+    rm ${PARAMS} ${PARAMS}c
 done
-mv -f "${PARAMS}.bak" "${PARAMS}"

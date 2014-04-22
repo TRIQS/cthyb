92bdfc2f2b34dc3ffefc33ef71918046585dc22f
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-04-22 17:46:42 +0200

    Adaptation of benchmark/nonint to the new solver.

--- benchmark/nonint/nonint.py
+++ benchmark/nonint/nonint.py
@@ -3,7 +3,7 @@
 import pytriqs.utility.mpi as mpi
 from pytriqs.archive import HDFArchive
 from pytriqs.parameters.parameters import Parameters
-from pytriqs.applications.impurity_solvers.cthyb_krylov import *
+from pytriqs.applications.impurity_solvers.cthyb import *
 from pytriqs.gf.local import *
 from pytriqs.plot.mpl_interface import *
 from matplotlib.backends.backend_pdf import PdfPages
@@ -17,79 +17,77 @@ print_master("This test is aimed to reveal excessive state truncation issues.")
 beta = 40.0
 
 for modes in range(1,10+1):
-	V = [0.2]*modes
-	e = [-0.2]*modes
+    V = [0.2]*modes
+    e = [-0.2]*modes
 
-	gf_struct = {str(n):[0] for n in range(0,len(V))}
+    gf_struct = {str(n):[0] for n in range(0,len(V))}
 
-	p = {}
-	p["beta"] = beta
-	p["max_time"] = -1
-	p["random_name"] = ""
-	p["random_seed"] = 123 * mpi.rank + 567
-	p["verbosity"] = 2
-	p["length_cycle"] = 50
-	p["n_warmup_cycles"] = 50000
-	p["n_cycles"] = 1200000
-	p["n_tau_delta"] = 1000
-        p["n_tau_g"] = 400
-        p["use_truncation"] = True
-        p["use_old_trace"] = False
-        p["use_quick_trace_estimator"] = False
-        p["trace_estimator_n_blocks_guess"] = -1
-	p["krylov_gs_energy_convergence"] = 1e-8
-	p["krylov_small_matrix_size"] = 100
+    p = {}
+    p["beta"] = beta
+    p["max_time"] = -1
+    p["random_name"] = ""
+    p["random_seed"] = 123 * mpi.rank + 567
+    p["verbosity"] = 2
+    p["length_cycle"] = 50
+    p["n_warmup_cycles"] = 50000
+    p["n_cycles"] = 1200000
+    p["n_tau_delta"] = 1000
+    p["n_tau_g"] = 400
+    p["measure_gt"] = True
+    p["measure_pert_order"] = False
+    p["make_histograms"] = False
+    p["trace_estimator"] = "FullTrace"
 
-	pp = Parameters()
-	for k in p: pp[k] = p[k]
+    pp = Parameters()
+    for k in p: pp[k] = p[k]
 
-	# Local Hamiltonian
-	H = Operator()
-	for n, b in enumerate(sorted(gf_struct.keys())):
-    		H += e[n]*N(b,0)
+    # Local Hamiltonian
+    H = Operator()
+    for n, b in enumerate(sorted(gf_struct.keys())):
+        H += e[n]*N(b,0)
 
-	# Quantum numbers (N_up and N_down)
-	QN=[]
-	for b in sorted(gf_struct.keys()): QN.append(N(b,0))
+    # Quantum numbers (N_up and N_down)
+    QN=[]
+    for b in sorted(gf_struct.keys()): QN.append(N(b,0))
 
-	print_master("Constructing the solver...")
+    print_master("Constructing the solver...")
 
-	# Construct the solver
-	S = Solver(parameters=pp, H_local=H, quantum_numbers=QN, gf_struct=gf_struct)
+    # Construct the solver
+    S = Solver(parameters=pp, H_local=H, quantum_numbers=QN, gf_struct=gf_struct)
 
-	print_master("Preparing the hybridization function...")
+    print_master("Preparing the hybridization function...")
 
-	# Set hybridization function
+    # Set hybridization function
     
-	for n, b in enumerate(sorted(gf_struct.keys())):
-    		delta_w = GfImFreq(indices = [0], beta=beta)
-    		delta_w <<= (V[n]**2) * inverse(iOmega_n - e[n])
-    		S.Delta_tau[b][0,0] <<= InverseFourier(delta_w)
+    for n, b in enumerate(sorted(gf_struct.keys())):
+        delta_w = GfImFreq(indices = [0], beta=beta)
+        delta_w <<= (V[n]**2) * inverse(iOmega_n - e[n])
+        S.Delta_tau[b][0,0] <<= InverseFourier(delta_w)
 
-	print_master("Running the simulation...")
+    print_master("Running the simulation...")
 
-	# Solve the problem
-	S.solve(parameters=pp)
+    # Solve the problem
+    S.solve(parameters=pp)
 
-	# Save and plot the results  
-	if mpi.rank==0:
-    		Results = HDFArchive('nonint_%i.h5'%len(V),'w')
-    		pdf = PdfPages("G_nonint_%i.pdf"%len(V))
+    # Save and plot the results  
+    if mpi.rank==0:
+        Results = HDFArchive('nonint_%i.h5'%len(V),'w')
+        pdf = PdfPages("G_nonint_%i.pdf"%len(V))
     
-    		for n, b in enumerate(sorted(gf_struct.keys())):
-    			Results['G_'+b] = S.G_tau[b]
+        for n, b in enumerate(sorted(gf_struct.keys())):
+            Results['G_'+b] = S.G_tau[b]
 
-    			g_theor = GfImTime(indices = [0], beta=beta, n_points=p["n_tau_g"])
-    			e1 = e[n] - V[n]
-    			e2 = e[n] + V[n]
-    			g_theor_w = GfImFreq(indices = [0], beta=beta)
-    			g_theor_w <<= 0.5*inverse(iOmega_n - e1) + 0.5*inverse(iOmega_n - e2)
-    			g_theor[0,0] <<= InverseFourier(g_theor_w)
+        g_theor = GfImTime(indices = [0], beta=beta, n_points=p["n_tau_g"])
+        e1 = e[n] - V[n]
+        e2 = e[n] + V[n]
+        g_theor_w = GfImFreq(indices = [0], beta=beta)
+        g_theor_w <<= 0.5*inverse(iOmega_n - e1) + 0.5*inverse(iOmega_n - e2)
+        g_theor[0,0] <<= InverseFourier(g_theor_w)
 
-    			plt.clf()
-			oplot(S.G_tau[b][0,0], name="Krylov")
-    			oplot(g_theor[0,0], name="Theory")
+        plt.clf()
+        oplot(S.G_tau[b][0,0], name="Krylov")
+        oplot(g_theor[0,0], name="Theory")
     
-    			pdf.savefig(plt.gcf())
+        pdf.savefig(plt.gcf())
     
-    		pdf.close()
+        pdf.close()

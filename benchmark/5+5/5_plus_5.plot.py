3aab706f83d9ac478e9a25380e770c691a6e6bd0
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-07-14 16:14:44 +0200

    5+5 benchmark: downsample G(\tau) before plotting

--- benchmark/5+5/5_plus_5.plot.py
+++ benchmark/5+5/5_plus_5.plot.py
@@ -8,6 +8,17 @@ from matplotlib.backends.backend_pdf import PdfPages
 from multiorbital import *
 from params import *
 
+# Super-lame solution
+def downsample(gf, new_n_tau):
+    new_gf = GfImTime(indices = range(len(gf.indices)+1), beta=gf.beta, n_points=new_n_tau)
+
+    sparse_points=[t for n,t in enumerate(new_gf.mesh)]
+    for n,t in enumerate(gf.mesh):
+        sparse_n = min(range(len(sparse_points)), key=lambda i: abs(sparse_points[i]-t))
+        new_gf.data[sparse_n] += gf.data[n]*float(len(new_gf.mesh))/float(len(gf.mesh))
+
+    return new_gf
+
 # Read the reference table file
 tau = []
 data = []
@@ -30,7 +41,7 @@ if not use_interaction:
 
         e1 = e - V
         e2 = e + V
-    
+
         g_theor_w = GfImFreq(indices = [0], beta=beta)
         g_theor_w <<= 0.5*inverse(iOmega_n - e1) + 0.5*inverse(iOmega_n - e2)
         g_theor[nc,nc] <<= InverseFourier(g_theor_w)
@@ -43,15 +54,17 @@ pp = PdfPages('G.pdf')
 for sn in spin_names:
     for nc, cn in enumerate(cubic_names):
         plt.clf()
-        
+
+        gf = downsample(arch[mkind(sn,cn)[0]],200)
+
         # Plot the results
-        oplot(arch[mkind(sn,cn)[0]], name="cthyb")
+        oplot(gf, name="cthyb")
         # Plot the reference curve
-	if use_interaction:
+        if use_interaction:
             oplot(g_ref[nc,nc], name="ED")
         else:
             oplot(g_theor[nc,nc], name="ED")
-        
+
         axes = plt.gca()
         axes.set_title('$' + sn + '$, $' + cn + '$')
         axes.set_xlabel('$\\tau$')

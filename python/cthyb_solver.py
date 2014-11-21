73309552fee290381298053bdd09f198d46d5a65
Priyanka Seth <pseth@cpht.polytechnique.fr>
2014-11-21 10:05:00 +0100

    Fix error in tail fitting

--- python/cthyb_solver.py
+++ python/cthyb_solver.py
@@ -33,9 +33,18 @@ class Solver(SolverCore):
         self.n_tau = n_tau
 
     def solve(self, h_loc, **params_kw):
-        """ Solve the impurity problem """
+        """
+        Solve the impurity problem.
+        If measure_g_tau (default = True), G_iw and Sigma_iw will be calculated and their tails fitted.
+        The tail fitting can be controlled by the following parameters:
+        :param fit_n_moments: (optional, default = 3) Number of moments to fit in the tail of Sigma_iw.
+        :param fit_known_moments: (optional, default = {block_name: TailGf(dim1, dim2, n_moments, order_min)})
+                                  Known moments of Sigma_iw, given as a TailGf object.
+        :param fit_min_n: (optional, default = int(0.8 * self.n_iw)) Index of iw from which to start fitting.
+        :param fit_max_n: (optional, default = n_iw) Index of iw to fit until.
+        """
 
-        # If tail parameters provided for G_iw fitting, use them, otherwise use defaults
+        # If tail parameters provided for Sigma_iw fitting, use them, otherwise use defaults
         fit_n_moments = params_kw.pop("fit_n_moments", 3)
         if "fit_known_moments" in params_kw:
             fit_known_moments = params_kw.pop("fit_known_moments")
@@ -43,8 +52,7 @@ class Solver(SolverCore):
             fit_known_moments = {}
             for name, indices in self.gf_struct.items():
                 dim = len(indices)
-                fit_known_moments[name] = TailGf(dim,dim,1,1) # TailGf(dim1, dim2, n_moments, order_min)
-                fit_known_moments[name][1] = identity(dim) # 1/w behaviour
+                fit_known_moments[name] = TailGf(dim,dim,0,0) # TailGf(dim1, dim2, n_moments, order_min)
         fit_min_n = params_kw.pop("fit_min_n", int(0.8 * self.n_iw)) # Fit last 20% of frequencies
         fit_max_n = params_kw.pop("fit_max_n", self.n_iw)
 
@@ -60,5 +68,7 @@ class Solver(SolverCore):
 
             # Solve Dyson's eq to obtain Sigma_iw and fit the tail
             self.Sigma_iw = inverse(self.G0_iw) - inverse(self.G_iw)
-            for name, g in self.Sigma_iw:
-                g.fit_tail(fit_known_moments[name], fit_n_moments, fit_min_n, fit_max_n)
+            for name, sig in self.Sigma_iw:
+                 self.Sigma_iw[name].fit_tail(fit_known_moments[name], fit_n_moments, fit_min_n, fit_max_n)
+                 # Now fix the tail of G_iw
+                 self.G_iw[name].tail = inverse( inverse(self.G0_iw[name].tail) - self.Sigma_iw[name].tail )

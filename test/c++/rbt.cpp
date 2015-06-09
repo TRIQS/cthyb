e39f2e70dba255831cc74752249151953a6edbe1
Igor Krivenko <igor.s.krivenko@gmail.com>
2015-06-09 11:29:28 +0200

    Updated and cleaned up rbt test.

--- test/c++/rbt.cpp
+++ test/c++/rbt.cpp
@@ -1,24 +1,29 @@
-#include "./rbt.hpp"
+#include <triqs/utility/rbt.hpp>
 #include <iostream>
 #include <fstream>
 #include <map>
-int main() { 
- rb_tree<int,int> tree;
 
- auto plot = [&tree]() {
- tree.graphviz(std::ofstream("g"));
- //system("dot -Tpdf g >g.pdf && open g.pdf");
- //std::cin.ignore();
+// We need this dummy wrapper type, because rb_tree is inherited
+// from its second template parameter, and C++ forbids inheritance
+// from fundamental types.
+struct int_ {
+  int i;
+  int_(int i = {}) : i(i) {}
+};
+
+int main() {
+ triqs::utility::rb_tree<int,int_> tree;
+
+ auto plot = [](auto & tree) {
+ tree.graphviz(std::cout);
  tree.clear_modified();
  };
-
-if (1) { 
 
  tree.insert(1,1);
  tree.clear_modified();
 
  tree.insert(7,7);
- plot();
+ plot(tree);
 
  tree.insert(3,3);
  tree.insert(5,5);
@@ -28,22 +33,21 @@ if (1) {
  tree.insert(10,0);
  tree.clear_modified();
  tree.insert(12,2);
-
- plot();
- //tree.graphviz(std::ofstream("g"));
+ plot(tree);
 
  tree.delete_node(7);
- plot();
+ plot(tree);
 
  tree.insert(7,7);
- plot();
+ plot(tree);
 
  tree.insert(6,6);
- plot();
+ plot(tree);
+
+ triqs::utility::rb_tree<int,int_> tree_copy(tree);
 
  tree.delete_node(10);
- plot();
-
+ plot(tree);
 
  tree.delete_node(1);
  tree.delete_node(3);
@@ -51,27 +55,24 @@ if (1) {
  //tree.delete_node(10);
  tree.delete_node(12);
  tree.delete_node(2);
+ plot(tree);
 
-}
- else { 
+ plot(tree_copy);
 
-std::map<int,int> t2;
-
-int N =1000;
-if (1) { 
- std::cout << " mon arbre"<< std::endl;
- for(int j=0; j<N; ++j)
-for(int i=0; i<N; ++i)
-tree.insert(i*N+j,j);
-
-}
-else {
- std::cout << "std map "<< std::endl; 
-for(int j=0; j<N; ++j)
-for(int i=0; i<N; ++i)
-t2.insert({i*N+j,j});
-}
-
-}
+//  std::map<int,int> t2;
+//
+//  int N =1000;
+//  if (1) {
+//   std::cout << " mon arbre"<< std::endl;
+//   for(int j=0; j<N; ++j)
+//   for(int i=0; i<N; ++i)
+//   tree.insert(i*N+j,j);
+//  }
+//  else {
+//   std::cout << "std map "<< std::endl;
+//   for(int j=0; j<N; ++j)
+//   for(int i=0; i<N; ++i)
+//   t2.insert({i*N+j,j});
+//  }
 
 }

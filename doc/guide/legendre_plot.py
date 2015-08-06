4c2bbf87e9d2e48d0ce3804aea430f1e343dcf1d
Igor Krivenko <igor.s.krivenko@gmail.com>
2015-08-06 20:39:17 +0200

    [doc] Open aim_solution.h5 in read-only mode.

--- doc/guide/legendre_plot.py
+++ doc/guide/legendre_plot.py
@@ -2,5 +2,5 @@ from pytriqs.gf.local import *
 from pytriqs.archive import *
 from pytriqs.plot.mpl_interface import oplot
 
-A = HDFArchive("aim_solution.h5")
+A = HDFArchive("aim_solution.h5",'r')
 oplot(A['G_l']['up'], '-o', x_window=(15,45) )

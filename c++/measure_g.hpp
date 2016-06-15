65478ce74ee93293a7fdd7dd14f9a0b57ca61e06
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-06-15 11:16:10 +0200

    [complex] Partially fix issue #46
    * Make code compile with HYBRIDISATION_IS_COMPLEX=OFF, LOCAL_HAMILTONIAN_IS_COMPLEX=ON.
    * The opposite combination of the flags is more complicated. One would need to measure all density matrices, including the atomic one, as complex if mc_weight_t = complex.

--- c++/measure_g.hpp
+++ c++/measure_g.hpp
@@ -28,15 +28,15 @@ using namespace triqs::gfs;
 
 // Measure imaginary time Green's function (one block)
 struct measure_g {
- 
+
  qmc_data const& data;
- gf_view<imtime, delta_target_t> g_tau;
+ gf_view<imtime, g_target_t> g_tau;
  int a_level;
  mc_weight_t z;
  int64_t num;
  mc_weight_t average_sign;
 
- measure_g(int a_level, gf_view<imtime, delta_target_t> g_tau, qmc_data const& data)
+ measure_g(int a_level, gf_view<imtime, g_target_t> g_tau, qmc_data const& data)
     : data(data), g_tau(g_tau), a_level(a_level) {
   g_tau() = 0.0;
   z = 0;
--- c++/solver_core.hpp
+++ c++/solver_core.hpp
@@ -43,7 +43,7 @@ class solver_core {
  many_body_op_t _h_loc;                         // The local Hamiltonian = h_int + h0
  block_gf<imfreq> _G0_iw;                       // Green's function containers: imaginary-freq Green's functions
  block_gf<imtime> _Delta_tau, _G_tau;           // Green's function containers: imaginary-time Green's functions
- block_gf<imtime, delta_target_t> _G_tau_accum; // Intermediate object to accumulate g(tau), either real or complex
+ block_gf<imtime, g_target_t> _G_tau_accum;     // Intermediate object to accumulate g(tau), either real or complex
  block_gf<legendre> _G_l;                       // Green's function containers: Legendre coefficients
  histogram _pert_order_total;                   // Histogram of the total perturbation order
  histo_map_t _pert_order;                       // Histograms of the perturbation order for each block

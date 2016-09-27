60b5eb7aadd35166874e744ee0f055ffea4f6deb
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-09-28 00:23:28 +0200

    [benchmark] Extend move_global plotting script to plot G(\tau)

--- benchmark/move_global/plot.py
+++ benchmark/move_global/plot.py
@@ -7,6 +7,9 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import numpy as np
 from sys import argv
+from itertools import product
+
+plot_n_tau = 400
 
 results_file = argv[1]
 
@@ -20,6 +23,7 @@ all_mg = arch.keys()
 all_mg.remove('beta')
 all_mg.remove('move_global_prob')
 
+# Plot occupations
 pp = PdfPages('occupation_mg_beta%.0f_prob%.2f.pdf' % (beta,move_global_prob))
 
 dat_N1_up = {mg: arch[mg]['N1_up'] for mg in all_mg}
@@ -48,6 +52,29 @@ plt.bar(index-0.13, y_N1_up, bottom=y_N1_dn, align='center', width=0.15, color =
 plt.bar(index+0.13, y_N2_up, bottom=y_N2_dn, align='center', width=0.15, color = 'tomato')
 plt.legend(loc = 'upper center', fontsize = 9)
 pp.savefig()
+
+pp.close()
+
+# Plot Green's functions
+pp = PdfPages('G_tau_mg_beta%.0f_prob%.2f.pdf' % (beta,move_global_prob))
+
+mg_pairs = [(a,b) for a,b in product(all_mg,all_mg) if a<b]
+spin_labels = {'up':'$\uparrow\uparrow$', 'dn':'$\downarrow\downarrow$'}
+for mg1, mg2 in mg_pairs:
+
+    plt.clf()
+    for s, i in product(('up','dn'),(0,1)):
+        G_tau_1 = rebinning_tau(arch[mg1]['G_tau'][s], plot_n_tau)
+        G_tau_2 = rebinning_tau(arch[mg2]['G_tau'][s], plot_n_tau)
+
+        oplot(G_tau_1[i,i], mode='R', label="%s,%s,%i%i" % (mg1,s,i,i))
+        oplot(G_tau_2[i,i], mode='R', label="%s,%s,%i%i" % (mg2,s,i,i))
+
+    plt.title("Test move_global $\\beta$ = %.0f, move_global_prob = %.3f" % (beta,move_global_prob), fontsize=12)
+    plt.ylim(-0.6,0.05)
+    plt.ylabel('$G(\\tau)$')
+    plt.legend(loc = 'lower center', fontsize = 9)
+    pp.savefig()
 
 pp.close()
 

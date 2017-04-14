deff9332d9feb4a6b1d18446b60f783458481f90
Hugo U. R. Strand <hugo.strand@gmail.com>
2017-04-14 09:10:32 +0200

    [measure_g2] g2_tau measurement code review HS+OP

--- c++/measures/g2_tau.cpp
+++ c++/measures/g2_tau.cpp
@@ -20,51 +20,40 @@
  ******************************************************************************/
 
 #include "./g2_tau.hpp"
-#include <cmath>
 
 namespace cthyb {
 
   using namespace triqs::gfs;
 
-  measure_g2_tau::measure_g2_tau(int A, int B, g2_view_type g2, qmc_data const &data) : data(data), g2(g2), A(A), B(B), diag_block(A == B) {
-
-    g2() = 0.0;
-    z    = 0;
-    num  = 0;
+  measure_g2_tau::measure_g2_tau(int A, int B, g2_tau_view_type g2_tau, qmc_data const &data)
+     : data(data), g2_tau(g2_tau), A(A), B(B), average_sign(0.0) {
+    g2_tau() = 0.0;
   }
 
-  void measure_g2_tau::accumulate(mc_weight_t s) {
-    num += 1;
-    if (num < 0) TRIQS_RUNTIME_ERROR << " Overflow of counter ";
+  void measure_g2_tau::accumulate(mc_weight_t sign) {
 
-    s *= data.atomic_reweighting;
-    z += s;
+    sign *= data.atomic_reweighting;
+    average_sign += sign;
 
-    auto const &det_A = data.dets[A];
-    auto const &det_B = data.dets[B];
-    if (det_A.size() == 0 || det_B.size() == 0) return;
+    foreach (data.dets[A], [&](auto const &j, auto const &i, auto const M_ij) {
+      foreach (data.dets[B], [&](auto const &l, auto const &k, auto const M_kl) {
 
-    using idx_t = std::pair<time_pt, int>;
-
-    foreach (data.dets[A], [&](idx_t const &j, idx_t const &i, det_scalar_t const M_ij) {
-      foreach (data.dets[B], [&](idx_t const &l, idx_t const &k, det_scalar_t const M_kl) {
-
-        auto add = [&](idx_t const &i, idx_t const &j, idx_t const &k, idx_t const &l, double sign) {
+        // lambda for computing a single product term of M_ij and M_kl
+        auto compute_M2_product = [&](auto const &i, auto const &j, auto const &k, auto const &l, double sign) {
 
           double t1 = double(i.first - l.first);
           double t2 = double(j.first - l.first);
           double t3 = double(k.first - l.first);
 
           // implicit beta-periodicity, but fix the sign properly
-          int sign_flips = int(i.first < l.first) + int(j.first < l.first) + int(k.first < l.first);
-          double factor  = (sign_flips % 2 ? -sign : sign);
+          int sign_flips    = int(i.first < l.first) + int(j.first < l.first) + int(k.first < l.first);
+          double pre_factor = (sign_flips % 2 ? -sign : sign);
 
-          this->g2[closest_mesh_pt(t1, t2, t3)](i.second, j.second, k.second, l.second) += factor * M_ij * M_kl;
+          this->g2_tau[closest_mesh_pt(t1, t2, t3)](i.second, j.second, k.second, l.second) += pre_factor * M_ij * M_kl;
         };
 
-
-        add(i, j, k, l, s);
-        if (A == B) add(i, l, k, j, -s);
+        compute_M2_product(i, j, k, l, sign);
+        if (A == B) compute_M2_product(i, l, k, j, -sign);
 
       })
         ;
@@ -72,63 +61,34 @@ namespace cthyb {
       ;
   }
 
-  void measure_g2_tau::collect_results(triqs::mpi::communicator const &c) {
+  void measure_g2_tau::collect_results(triqs::mpi::communicator const &comm) {
 
-    z = mpi_all_reduce(z, c);
-    g2 = mpi_all_reduce(g2, c);
+    average_sign = mpi_all_reduce(average_sign, comm);
+    g2_tau       = mpi_all_reduce(g2_tau, comm);
 
-    // Fixme: use product reduction on delta()
-    double dtau0 = std::get<0>(g2.mesh().components()).delta();
-    double dtau1 = std::get<1>(g2.mesh().components()).delta();
-    double dtau2 = std::get<2>(g2.mesh().components()).delta();
-    double dtau3 = dtau0 * dtau1 * dtau2;
+    // Bin volime in imaginary time space
+    double dtau0    = std::get<0>(g2_tau.mesh()).delta();
+    double dtau1    = std::get<1>(g2_tau.mesh()).delta();
+    double dtau2    = std::get<2>(g2_tau.mesh()).delta();
+    double dtau_vol = dtau0 * dtau1 * dtau2;
 
-    g2 = g2 / (real(z) * data.config.beta() * dtau3); // Q: tau mesh delta tau?
+    // Rescale sampled Green's function
+    g2_tau = g2_tau / (real(average_sign) * data.config.beta() * dtau_vol);
 
-    // Account for edge bins beeing smaller
+    // Account for
+    // the 1/2 smaller volume of the side bins,
+    // the 1/4 smaller volume of the edge bins, and
+    // the 1/8 smaller volume of the corner bins.
+
     auto _ = var_t{};
-    int n  = std::get<0>(g2.mesh().components()).size() - 1;
+    int n  = std::get<0>(g2_tau.mesh().components()).size() - 1;
 
-    // There are 6 sides of the cube
-    
-    g2[0][_][_] *= 2.0;
-    g2[_][0][_] *= 2.0;
-    g2[_][_][0] *= 2.0;
+    g2_tau[0][_][_] *= 2.0;
+    g2_tau[_][0][_] *= 2.0;
+    g2_tau[_][_][0] *= 2.0;
 
-    g2[n][_][_] *= 2.0;
-    g2[_][n][_] *= 2.0;
-    g2[_][_][n] *= 2.0;
-
-    /*
-    // There are 3*4 = 12 edges of the cube
-
-    g2[0][0][_] *= 2.0;
-    g2[_][0][0] *= 2.0;
-    g2[0][_][0] *= 2.0;
-
-    g2[n][n][_] *= 2.0;
-    g2[_][n][n] *= 2.0;
-    g2[n][_][n] *= 2.0;
-
-    g2[n][0][_] *= 2.0;
-    g2[_][n][0] *= 2.0;
-    g2[n][_][0] *= 2.0;
-
-    g2[0][n][_] *= 2.0;
-    g2[_][0][n] *= 2.0;
-    g2[0][_][n] *= 2.0;
-
-    // there are 8 corners of the cube
-
-    g2[0][0][0] *= 2.0; // 1
-    g2[n][0][0] *= 2.0; // 2
-    g2[0][n][0] *= 2.0; // 3
-    g2[0][0][n] *= 2.0; // 4
-    g2[n][n][0] *= 2.0; // 5
-    g2[n][0][n] *= 2.0; // 6
-    g2[0][n][n] *= 2.0; // 7
-    g2[n][n][n] *= 2.0; // 8
-    */
-    
-  }  
+    g2_tau[n][_][_] *= 2.0;
+    g2_tau[_][n][_] *= 2.0;
+    g2_tau[_][_][n] *= 2.0;
+  }
 }

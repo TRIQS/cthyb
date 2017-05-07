ab81f9475cf4de2e72dc3ad083f24eb2e8774345
Igor Krivenko <igor.s.krivenko@gmail.com>
2017-05-06 20:17:34 -0400

    [benchmark] Use less extreme parameters in measure_g2

--- benchmark/measure_g2/measure_g2.py
+++ benchmark/measure_g2/measure_g2.py
@@ -5,43 +5,49 @@ from pytriqs.operators.util.op_struct import set_operator_structure, get_mkind
 from pytriqs.operators.util.hamiltonians import h_int_kanamori
 from pytriqs.applications.impurity_solvers.cthyb import *
 from pytriqs.applications.impurity_solvers.cthyb.util import estimate_nfft_buf_size
-from pytriqs.gf.local import *
+from pytriqs.gf import *
 import numpy as np
 
 # Input parameters
 beta = 10.0
 num_orb = 2
-mu = 1.0
+mu = 1.5
 U = 2.0
 J = 0.2
-epsilon = [-2.3,2.3]
-V = [1.0*np.eye(num_orb) + 0.1*(np.ones((num_orb,num_orb)) - np.eye(num_orb))]*2
+epsilon = [-1.3, 1.3]
+V = [2.0*np.eye(num_orb) + 0.2*(np.ones((num_orb, num_orb)) - np.eye(num_orb))]*2
 
-spin_names = ("up","dn")
+spin_names = ("up", "dn")
 orb_names = range(num_orb)
 n_iw = 1024
 
 g2_n_iw = 5
 g2_n_inu = 10
 g2_n_l = 4
-g2_blocks = set([("up","up"),("up","dn"),("dn","up")])
+g2_blocks = set([("up", "up"), ("up", "dn"), ("dn", "up")])
 
 p = {}
 p["verbosity"] = 2
 p["max_time"] = -1
 p["random_name"] = ""
-p["random_seed"] = 123 * mpi.rank + 567
+p["random_seed"] = 12345 * mpi.rank + 567
 p["length_cycle"] = 50
 p["n_warmup_cycles"] = 50000
-p["n_cycles"] = 500
-p["use_norm_as_weight"] = True
+p["n_cycles"] = 5000
+p["use_norm_as_weight"] = False
+p["move_double"] = True
 p["measure_density_matrix"] = False
 p["measure_g_tau"] = False
 p["measure_g_l"] = False
+p["measure_pert_order"] = False
 
 # Parameters for the preliminary run
+preliminary_only = False # Skip G2 accumulation?
 p_pre = p.copy()
+p_pre["n_warmup_cycles"] = 50000
+p_pre["n_cycles"] = 500000
 p_pre["measure_pert_order"] = True
+p_pre["measure_g_tau"] = True
 
 p["measure_g2_inu"] = True
 p["measure_g2_legendre"] = True
@@ -55,46 +61,60 @@ p["measure_g2_n_l"] = g2_n_l
 
 mpi.report("Welcome to measure_g2 benchmark.")
 
-gf_struct = set_operator_structure(spin_names,orb_names,True)
-mkind = get_mkind(True,None)
+gf_struct = set_operator_structure(spin_names, orb_names, True)
+mkind = get_mkind(True, None)
 
 # Hamiltonian
-H = h_int_kanamori(spin_names,orb_names,
-                   np.array([[0,U-3*J],[U-3*J,0]]),
-                   np.array([[U,U-2*J],[U-2*J,U]]),
-                   J,True)
+H = h_int_kanamori(spin_names, orb_names,
+                   np.array([[0, U-3*J], [U-3*J, 0]]),
+                   np.array([[U, U-2*J], [U-2*J, U]]),
+                   J, True)
 
 mpi.report("Constructing the solver...")
 
 # Construct the solver
-S = SolverCore(beta=beta, gf_struct=gf_struct, n_iw=n_iw)
+S = SolverCore(beta = beta, gf_struct = gf_struct, n_iw = n_iw)
 
 mpi.report("Preparing the hybridization function...")
 
 # Set hybridization function
-delta_w = GfImFreq(indices = orb_names, beta=beta, n_points=n_iw)
+delta_w = GfImFreq(indices = orb_names, beta = beta, n_points = n_iw)
 delta_w_part = delta_w.copy()
 for e, v in zip(epsilon,V):
     delta_w_part << inverse(iOmega_n - e)
-    delta_w_part.from_L_G_R(np.transpose(v),delta_w_part,v)
+    delta_w_part.from_L_G_R(np.transpose(v), delta_w_part, v)
     delta_w += delta_w_part
 
 S.G0_iw << inverse(iOmega_n + mu - delta_w)
 
-# Solve the problem
+# Accumulate histograms and G_tau
 mpi.report("Preliminary run...")
-S.solve(h_int=H, **p_pre)
+S.solve(h_int = H, **p_pre)
 
-p["nfft_buf_sizes"] = estimate_nfft_buf_size(gf_struct, S.perturbation_order)
+if mpi.is_master_node():
+    with HDFArchive("measure_g2.h5", 'w') as ar:
+        ar['beta'] = beta
+        ar['U'] = U
+        ar['mu'] = mu
+        ar['J'] = J
+        ar['epsilon'] = epsilon
+        ar['V'] = V
+        ar['G_tau'] = S.G_tau
+        ar['pre_solve_params'] = p_pre
 
+if preliminary_only: exit()
+
+# Accumulate G2
 mpi.report("Running the simulation...")
-S.solve(h_int=H, **p)
+pert_order = S.perturbation_order.copy()
+p["nfft_buf_sizes"] = estimate_nfft_buf_size(gf_struct, S.perturbation_order)
+S.solve(h_int = H, **p)
 
 # Check shapes of g2 containers
-ref_shape_inu = (2*g2_n_iw-1,2*g2_n_inu,2*g2_n_inu,
-                 num_orb,num_orb,num_orb,num_orb)
-ref_shape_l = (2*g2_n_iw-1,g2_n_l,g2_n_l,
-               num_orb,num_orb,num_orb,num_orb)
+ref_shape_inu = (2*g2_n_iw-1, 2*g2_n_inu, 2*g2_n_inu,
+                 num_orb, num_orb, num_orb, num_orb)
+ref_shape_l = (2*g2_n_iw-1, g2_n_l, g2_n_l,
+               num_orb, num_orb, num_orb, num_orb)
 for bn in g2_blocks:
     assert S.G2_iw_inu_inup_pp[bn].data.shape == ref_shape_inu
     assert S.G2_iw_inu_inup_ph[bn].data.shape == ref_shape_inu
@@ -103,13 +123,7 @@ for bn in g2_blocks:
 
 # Save the results
 if mpi.is_master_node():
-    with HDFArchive("measure_g2.h5",'w') as ar:
-        ar['beta'] = beta
-        ar['U'] = U
-        ar['mu'] = mu
-        ar['J'] = J
-        ar['epsilon'] = epsilon
-        ar['V'] = V
+    with HDFArchive("measure_g2.h5", 'a') as ar:
         p['measure_g2_blocks'] = list(p['measure_g2_blocks'])
         ar['solve_params'] = p
         ar['G2_iw_inu_inup_pp'] = S.G2_iw_inu_inup_pp

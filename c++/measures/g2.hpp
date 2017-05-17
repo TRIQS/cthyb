6a51ce699da9f6ed7228fbd77b9bbd00ad14ed2e
Hugo U. R. Strand <hugo.strand@gmail.com>
2017-05-17 14:16:31 +0200

    [measure] g2_inu refactoring wip

--- c++/measures/g2.hpp
+++ c++/measures/g2.hpp
@@ -33,28 +33,51 @@ namespace cthyb {
   using namespace triqs::arrays;
 
   // Measure one block of G^2(iomega,inu,inu')
-  template <g2_channel Channel, block_order Order> struct measure_g2_inu {
+  template <g2_channel Channel> struct measure_g2_inu {
 
     using g2_view_type = gf_view<cartesian_product<imfreq, imfreq, imfreq>, tensor_valued<4>>;
+    using M_type = gf<cartesian_product<imfreq, imfreq>, matrix_valued>;
+    using M_block_type = block_gf<cartesian_product<imfreq, imfreq>, matrix_valued>;
 
     qmc_data const &data;
     g2_view_type g2;
     const int A;           // Index of block A within gf_struct
     const int B;           // Index of block B within gf_struct
     const bool diag_block; // A == B
-    mc_weight_t z;
-    int64_t num;
+    mc_weight_t average_sign;
+    block_order order;
 
     // Objects that perform 2D NFFT transforms
     nfft_array_t<2, 2> nfft_M_ab, nfft_M_cd;
     nfft_array_t<2, 2> nfft_M_ad, nfft_M_cb;
 
     // Results of NFFT transforms
-    gf<cartesian_product<imfreq, imfreq>, matrix_valued> M_ab, M_cd, M_ad, M_cb;
+    M_type M_ab, M_cd, M_ad, M_cb;
 
-    measure_g2_inu(int A, int B, g2_view_type g2, qmc_data const &data, int buf_size_A, int buf_size_B);
+    M_block_type M;
+    array<nfft_array_t<2, 2>, 1> M_nfft;
+
+    measure_g2_inu(int A, int B, g2_view_type g2, qmc_data const &data, int buf_size_A, int buf_size_B, block_order order, gf_struct_t const & gf_struct);
     void accumulate(mc_weight_t s);
     void collect_results(triqs::mpi::communicator const &c);
+
+    inline void accumulate_impl_AABB(mc_weight_t s, M_type const & M_ab, M_type const & M_cd);
+    inline void accumulate_impl_ABBA(mc_weight_t s, M_type const & M_ad, M_type const & M_cb);
+
+    // Frequency placeholders
+    clef::placeholder<0> iw_;
+    clef::placeholder<1> inu_;
+    clef::placeholder<2> inup_;
+
+    // Index placeholders
+    clef::placeholder<3> a_;
+    clef::placeholder<4> b_;
+    clef::placeholder<5> c_;
+    clef::placeholder<6> d_;
+
+    clef::placeholder<0> w1;
+    clef::placeholder<1> w2;
+    clef::placeholder<2> w3;
   };
 
   // Measure one block of G^2(iomega,l,l')

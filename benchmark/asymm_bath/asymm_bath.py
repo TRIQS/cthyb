02624f34cbb0b76ef9e28b851f3fa7c870d4460f
Hugo U. R. Strand <hugo.strand@gmail.com>
2017-07-25 11:09:23 +0200

    [benchmark] asymm_bath

--- benchmark/asymm_bath/asymm_bath.py
+++ benchmark/asymm_bath/asymm_bath.py
@@ -1,7 +1,7 @@
 #!/bin/env pytriqs
 
 import pytriqs.utility.mpi as mpi
-from pytriqs.gf.local import *
+from pytriqs.gf import *
 from pytriqs.operators import *
 from pytriqs.archive import HDFArchive
 from pytriqs.applications.impurity_solvers.cthyb import *

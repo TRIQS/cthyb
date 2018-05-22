41b7253de7f48962154a0c00b6c9efa17804a231
Olivier Parcollet <olivier.parcollet@cea.fr>
2018-05-22 21:52:27 +0200

    [doc] Doc upgrade

--- doc/guide/slater_five_band_plot.py
+++ doc/guide/slater_five_band_plot.py
@@ -8,7 +8,7 @@ with HDFArchive('slater_five_band.h5','r') as ar:
     G_tau = ar['G_tau-1']
     g = G_tau['up_0']
     # This gives a complex valued gf (required by rebinning_tau)
-    g_tau_ave = Gf(mesh=g.mesh, target_shape=g.target_shape, singularity=g.singularity)
+    g_tau_ave = Gf(mesh=g.mesh, target_shape=g.target_shape)
     for name, g in G_tau: g_tau_ave += g
     g_tau_ave = g_tau_ave/10.
     g_tau_rebin = rebinning_tau(g_tau_ave,1000)

12a464cca35dc98f93e0bb4b269a9c32c518dfda
Hugo U. R. Strand <hugo.strand@gmail.com>
2019-02-07 17:33:36 -0500

    [bench] kanamori_offdiag move_double=True

--- benchmark/kanamori_offdiag/kanamori_offdiag.py
+++ benchmark/kanamori_offdiag/kanamori_offdiag.py
@@ -21,67 +21,67 @@ V = [1.0*np.eye(num_orbitals) + 0.1*(np.ones((num_orbitals,num_orbitals)) - np.e
 spin_names = ("up","dn")
 orb_names = range(num_orbitals)
 
-# Use quantum numbers
-use_qn = False
+for use_qn in (True, False):
 
-n_iw = 1025
-n_tau = 10001
+    n_iw = 1025
+    n_tau = 10001
 
-p = {}
-p["max_time"] = -1
-p["random_name"] = ""
-p["random_seed"] = 123 * mpi.rank + 567
-p["length_cycle"] = 50
-p["n_warmup_cycles"] = 50000
-p["n_cycles"] = 3200000
-p["use_norm_as_weight"] = True
-p["measure_density_matrix"] = False
+    p = {}
+    p["max_time"] = -1
+    p["random_name"] = ""
+    p["random_seed"] = 123 * mpi.rank + 567
+    p["length_cycle"] = 100
+    p["n_warmup_cycles"] = 50000
+    p["n_cycles"] = int(1.e7 / mpi.size)
+    p["use_norm_as_weight"] = True
+    p["measure_density_matrix"] = False
+    p["move_double"] = True
 
-results_file_name = "kanamori_offdiag." + ("qn." if use_qn else "") + "h5"
+    results_file_name = "kanamori_offdiag." + ("qn." if use_qn else "") + "h5"
 
-mpi.report("Welcome to Kanamori (off-diagonal) benchmark.")
+    mpi.report("Welcome to Kanamori (off-diagonal) benchmark.")
 
-gf_struct = set_operator_structure(spin_names,orb_names,True)
-mkind = get_mkind(True,None)
+    gf_struct = set_operator_structure(spin_names,orb_names,True)
+    mkind = get_mkind(True,None)
 
-# Hamiltonian
-H = h_int_kanamori(spin_names,orb_names,
-                   np.array([[0,U-3*J],[U-3*J,0]]),
-                   np.array([[U,U-2*J],[U-2*J,U]]),
-                   J,True)
+    # Hamiltonian
+    H = h_int_kanamori(spin_names,orb_names,
+                       np.array([[0,U-3*J],[U-3*J,0]]),
+                       np.array([[U,U-2*J],[U-2*J,U]]),
+                       J,True)
 
-if use_qn:
-    QN = [sum([n(*mkind("up",o)) for o in orb_names],Operator()),
-          sum([n(*mkind("dn",o)) for o in orb_names],Operator())]
-    for o in orb_names:
-        dn = n(*mkind("up",o)) - n(*mkind("dn",o))
-        QN.append(dn*dn)
-    p["partition_method"] = "quantum_numbers"
-    p["quantum_numbers"] = QN
+    if use_qn:
+        QN = [sum([n(*mkind("up",o)) for o in orb_names],Operator()),
+              sum([n(*mkind("dn",o)) for o in orb_names],Operator())]
+        for o in orb_names:
+            dn = n(*mkind("up",o)) - n(*mkind("dn",o))
+            QN.append(dn*dn)
+        p["partition_method"] = "quantum_numbers"
+        p["quantum_numbers"] = QN
 
-mpi.report("Constructing the solver...")
+    mpi.report("Constructing the solver...")
 
-# Construct the solver
-S = SolverCore(beta=beta, gf_struct=gf_struct, n_tau=n_tau, n_iw=n_iw)
+    # Construct the solver
+    S = SolverCore(beta=beta, gf_struct=gf_struct, n_tau=n_tau, n_iw=n_iw)
 
-mpi.report("Preparing the hybridization function...")
+    mpi.report("Preparing the hybridization function...")
 
-# Set hybridization function
-delta_w = GfImFreq(indices = orb_names, beta=beta, n_points=n_iw)
-delta_w_part = delta_w.copy()
-for e, v in zip(epsilon,V):
-    delta_w_part << inverse(iOmega_n - e)
-    delta_w_part.from_L_G_R(np.transpose(v),delta_w_part,v)
-    delta_w += delta_w_part
+    # Set hybridization function
+    delta_w = GfImFreq(indices = orb_names, beta=beta, n_points=n_iw)
+    delta_w_part = delta_w.copy()
+    for e, v in zip(epsilon,V):
+        delta_w_part << inverse(iOmega_n - e)
+        delta_w_part.from_L_G_R(np.transpose(v),delta_w_part,v)
+        delta_w += delta_w_part
 
-S.G0_iw << inverse(iOmega_n + mu - delta_w)
+    S.G0_iw << inverse(iOmega_n + mu - delta_w)
 
-mpi.report("Running the simulation...")
+    mpi.report("Running the simulation...")
 
-# Solve the problem
-S.solve(h_int=H, **p)
+    # Solve the problem
+    S.solve(h_int=H, **p)
 
-# Save the results
-if mpi.is_master_node():
-    with HDFArchive(results_file_name,'w') as Results:
-        Results['G_tau'] = S.G_tau
+    # Save the results
+    if mpi.is_master_node():
+        with HDFArchive(results_file_name,'w') as Results:
+            Results['G_tau'] = S.G_tau
---
+++ benchmark/kanamori_offdiag/pyed_kanamori_offdiag.py
@@ -0,0 +1,90 @@
+
+import itertools
+import numpy as np
+from scipy.linalg import block_diag
+
+from pytriqs.archive import HDFArchive
+from pytriqs.operators import c, c_dag, n
+
+from pytriqs.gf import Gf
+from pytriqs.gf import MeshImTime, MeshImFreq
+from pytriqs.operators.util.hamiltonians import h_int_kanamori
+
+from pyed.OperatorUtils import get_quadratic_operator
+from pyed.OperatorUtils import fundamental_operators_from_gf_struct
+from pyed.TriqsExactDiagonalization import TriqsExactDiagonalization
+
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+
+    norb = 2
+    spin_names = ['up', 'do']
+    imp_idxs = list(range(0, norb))
+    bath_idxs = list(range(norb, 3*norb))
+    
+    fop_imp = fundamental_operators_from_gf_struct(
+        [ ['up', imp_idxs], ['do', imp_idxs] ])
+
+    fop_bath = fundamental_operators_from_gf_struct(
+        [ ['up', bath_idxs], ['do', bath_idxs] ])
+
+    print spin_names
+    print imp_idxs
+    print bath_idxs
+    print fop_imp
+    print fop_bath
+    
+    fop = fop_imp + fop_bath
+
+    beta = 10.0
+    mu = 1.0
+    U = 2.0
+    J = 0.2
+
+    V = 1.0*np.eye(norb) + 0.1*(np.ones((norb, norb)) - np.eye(norb))
+    V = np.hstack([V, V])
+    
+    T_imp = -mu * np.eye(norb)
+    
+    T_bath = np.diag([-2.3, -2.3, 2.3, 2.3])
+ 
+    print 'V =\n', V
+    print 'T_imp =\n', T_imp
+    print 'T_bath =\n', T_bath
+
+    V = block_diag(V, V)
+    T_imp = block_diag(T_imp, T_imp)
+    T_bath = block_diag(T_bath, T_bath)
+
+    print 'V =\n', V
+    print 'T_imp =\n', T_imp
+    print 'T_bath =\n', T_bath
+    
+    T = np.block([
+        [ T_imp, V ],
+        [ V.T, T_bath]
+        ])
+    
+    print 'T =\n', T
+
+    H_int = h_int_kanamori(
+        spin_names, imp_idxs,
+        np.array([[0,U-3*J],[U-3*J,0]]),
+        np.array([[U,U-2*J],[U-2*J,U]]),
+        J,True)
+    
+    H = H_int + get_quadratic_operator(T, fop)
+            
+    ed = TriqsExactDiagonalization(H, fop, beta)
+
+    n_tau = 101    
+    G_tau_up = Gf(mesh=MeshImTime(beta, 'Fermion', n_tau), target_shape=[2, 2])
+    G_tau_do = Gf(mesh=MeshImTime(beta, 'Fermion', n_tau), target_shape=[2, 2])
+
+    for i1, i2 in itertools.product(range(norb), repeat=2):
+        ed.set_g2_tau(G_tau_up[i1, i2], c('up', i1), c_dag('up', i2))
+        ed.set_g2_tau(G_tau_do[i1, i2], c('do', i1), c_dag('do', i2))
+
+    with HDFArchive('kanamori_offdiag.pyed.h5', 'w') as Results:
+        Results['up'] = G_tau_up
+        Results['dn'] = G_tau_do

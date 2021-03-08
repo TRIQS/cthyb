91c2f7713783ced7d99d80b7601644a6fc4e015a
Jonathan Karp <jkarp314@gmail.com>
2021-03-08 12:13:58 -0500

    Update c++/triqs_cthyb/parameters.cpp
    Co-authored-by: Nils Wentzell <Wentzell@users.noreply.github.com>

--- c++/triqs_cthyb/parameters.cpp
+++ c++/triqs_cthyb/parameters.cpp
@@ -211,7 +211,6 @@ namespace triqs_cthyb {
     h5_read(grp, "det_precision_warning", sp.det_precision_warning);
     h5_read(grp, "det_precision_error", sp.det_precision_error);
     h5_read(grp, "det_singular_threshold", sp.det_singular_threshold);
-
     h5_try_read(grp, "Delta_infty", sp.Delta_infty);
 
   }

7f82c5ffdde5ae27b296b8cde26d4355c50886fb
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-06-10 17:07:58 -0400

    Add backward compatibility layer for h5::read of gf_struct

--- c++/triqs_cthyb/parameters.cpp
+++ c++/triqs_cthyb/parameters.cpp
@@ -76,18 +76,7 @@ namespace triqs_cthyb {
     h5_read(grp, "n_tau", cp.n_tau);
     h5_read(grp, "n_l", cp.n_l);
     h5_try_read(grp, "Delta_interface", cp.Delta_interface);
-
-    // Read gf_struct with backward compatibility layer for old gf_struct type
-    auto gf_struct_0 = grp.open_group("gf_struct").open_group("0");
-    if (gf_struct_0.has_subgroup("1")){
-      auto gf_struct_bkwd = std::vector<std::pair<std::string, std::vector<std::variant<int, std::string>>>>{};
-      h5_read(grp, "gf_struct", gf_struct_bkwd);
-      cp.gf_struct.clear();
-      for(auto const & [k, v]: gf_struct_bkwd)
-	cp.gf_struct.push_back({k, v.size()});
-    } else {
-      h5_read(grp, "gf_struct", cp.gf_struct);
-    }
+    triqs::gfs::h5_read_gf_struct(grp, "gf_struct", cp.gf_struct);
   }
 
   void h5_write(h5::group h5group, std::string name, solve_parameters_t const &sp) {

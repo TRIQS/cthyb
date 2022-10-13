2da3f97e50bfc8829a08bf0096db08b1106b6c94
Harrison LaBollita <hlabolli@asu.edu>
2022-10-13 14:47:54 -0400

    calculate high freq moments of Sigma and G from density matrix
    - functions to calculate high freq moments of G and Sigma
    - moments calculated when density matrix is measured
    - moments used in transform of Gtau and tail fit of Sigma
    - python test for high freq moments

--- python/triqs_cthyb/tail_fit.py
+++ python/triqs_cthyb/tail_fit.py
@@ -24,6 +24,90 @@ import numpy as np
 
 from triqs.gf.gf_fnt import fit_hermitian_tail_on_window, replace_by_tail
 
+from triqs.operators import c, c_dag
+from triqs.atom_diag import trace_rho_op
+
+
+def sigma_high_frequency_moments(density_matrix,
+                           h_loc, 
+                           gf_struct, 
+                           h_int):
+    """
+    Calculate the first and second analytic high frequency moments of Sigma_iw
+    following Rev. Mod. Phys. 83, 349 (2011), where the first and second moments are
+    (1) Sigma_Hartree = -<{[H,c],c+}>
+    (2) Sigma_1       = <{[H,[H,c]],c+}> - Sigma_Hartree^2,
+    where H is th interaction Hamiltonian
+
+    Parameters
+    ----------
+    density_matrix : list, np.ndarray
+                     measured density matrix from TRIQS/CTHYB.
+    h_loc          : h_loc_diagonalization
+                     h_loc_diagonalizatoin from TRIQS/CTHYB.
+    gf_struct      : gf_struct_t
+                     Block structure of Green's function.
+    h_int          : H_int
+                     interaction Hamiltonian
+
+    Returns
+    -------
+    moments        : list
+                     first and second moments in a list with the
+                     same structure as the Green's function
+    """
+    def comm(A,B): return A*B - B*A
+    def anticomm(A,B): return A*B + B*A
+
+    # Sigma_HF term
+    sigma_hf = {bl : np.zeros((bl_size, bl_size),dtype=complex) for bl, bl_size in gf_struct}
+    for bl, bl_size in gf_struct:
+        for orb1 in range(bl_size):
+            for orb2 in range(bl_size):
+                op = -anticomm(comm(h_int, c(bl,orb1)), c_dag(bl,orb2))
+                for term, coef in op:
+                    bl1, u1 = term[0][1]
+                    bl2, u2 = term[1][1]
+
+                    sigma_hf[bl][orb1,orb2] += coef*trace_rho_op(density_matrix,
+                                                                 c_dag(bl1,u1)*c(bl2,u2),
+                                                                 h_loc
+                                                                 )
+    # Sigma_1/iwn term
+    sigma_1 = {bl : np.zeros((bl_size,bl_size),dtype=complex) for bl,bl_size in gf_struct}
+    for bl, bl_size in gf_struct:
+        for orb1 in range(bl_size):
+            for orb2 in range(bl_size):
+                op = anticomm(comm(h_int, comm(h_int, c(bl,orb1))), c_dag(bl,orb2))
+                for term, coef in op:
+
+                    if len(term) == 2:
+
+                        bl1, u1 = term[0][1]
+                        bl2, u2 = term[1][1]
+
+                        sigma_1[bl][orb1,orb2] += coef*trace_rho_op(density_matrix,
+                                                                    c_dag(bl1,u1)*c(bl2,u2),
+                                                                    h_loc
+                                                                    )
+                    elif len(term) == 4:
+
+                        bl1, u1 = term[0][1]
+                        bl2, u2 = term[1][1]
+                        bl3, u3 = term[2][1]
+                        bl4, u4 = term[3][1]
+
+                        sigma_1[bl][orb1,orb2] += coef*trace_rho_op(density_matrix,
+                                                                    c_dag(bl1,u1)*c_dag(bl2,u2)*c(bl3,u3)*c(bl4,u4),
+                                                                    h_loc
+                                                                    )
+
+                sigma_1[bl][orb1,orb2] -= sigma_hf[bl][orb1,orb2]**2
+
+    return [sigma_hf, sigma_1]
+
+
+
 def tail_fit(
         Sigma_iw,
         fit_min_n=None, fit_max_n=None,
--- python/triqs_cthyb/util.py
+++ python/triqs_cthyb/util.py
@@ -24,6 +24,10 @@ CTHYB utility functions:
 from math import ceil
 from numpy import argmax
 
+import numpy as np
+from triqs.operators import c, c_dag
+from triqs.atom_diag import trace_rho_op
+
 def block_size_from_gf_struct(block_name, gf_struct):
     bns, idxs = list(zip(*gf_struct))
     bidx = bns.index(block_name)
@@ -41,4 +45,16 @@ def estimate_nfft_buf_size(gf_struct, pert_order_histograms):
             buf_sizes[bn] = int(max(ceil((max_order * max_order) / (block_size * block_size)), 1))
 
     return buf_sizes
+
+def orbital_occupations(density_matrix, gf_struct, h_loc_diag):
+    
+    dtype=density_matrix[0].dtype
+    occ_mat = {bl: np.zeros((bl_size,bl_size), dtype=dtype) for bl, bl_size in gf_struct}
+
+    for bl, bl_size in gf_struct:
+        for i in range(bl_size):
+            for j in range(bl_size):
+                occ_mat[bl][i,j] = trace_rho_op(density_matrix, c_dag(bl, i)*c(bl,j), h_loc_diag)
+
+    return occ_mat
 

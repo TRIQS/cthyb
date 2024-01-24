c32599fe8a2e199d72df13bd61a5902ae74ea879
Olivier Parcollet <oparcollet@flatironinstitute.org>
2024-01-24 17:09:10 -0500

    FIX

--- python/triqs_cthyb/solver_core.cpp
+++ python/triqs_cthyb/solver_core.cpp
@@ -7,17 +7,11 @@
 namespace c2py_module {
 
   auto documentation = "The TRIQS cthyb solver";
-  auto ns            = "triqs_cthyb";
   auto package_name  = "triqs_cthyb";
 
   auto get_set_as_properties = true;
 
-  auto regex_filter =  "triqs_cthyb::(solver_core|solve_parameters_t|constr_parameters_t|block_order)";
-
-  // Alternatively, one can be more specific fun/cls/enum... Same result
-  //auto regex_exclude_fun  = ".*"; 
-  //auto regex_filter_cls  = "triqs_cthyb::(solver_core|solve_parameters_t|constr_parameters_t)";
-  //auto regex_filter_enum = "triqs_cthyb::block_order";
+  auto match_names =  "triqs_cthyb::(solver_core|solve_parameters_t|constr_parameters_t|block_order)";
 
 
 } // namespace c2py_module

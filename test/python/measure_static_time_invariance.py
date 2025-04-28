96b98494e016a2afdeedf1435043d192a156a39e
Emmanuel CASTIEL <Emmanuel.CASTIEL@cea.fr>
2025-04-28 17:16:45 +0200

    Update test on time invariance

--- test/python/measure_static_time_invariance.py
+++ test/python/measure_static_time_invariance.py
@@ -12,7 +12,8 @@ beta = 10.0
 U = 2.0
 mu = 1.0
 h = 0.1
-V = 1.0
+norb = 2
+V = 1.0 * np.eye(norb) + 0.1 * (np.ones(norb) - np.eye(norb))
 t = 0.1
 epsilon = 2.3
 
@@ -37,6 +38,10 @@ gm['swap_orbs']  = {("up",0) : ("up",1), ("up",1) : ("up",0), ("dn",0) : ("dn",1
 p["move_global"] = gm
 p["move_global_prob"] = 0.06
 
+qn = [n("up",0) + n("up",1),n("dn",0) + n("dn",1)]
+p["quantum_numbers"] = qn
+p["partition_method"] = "quantum_numbers"
+
 H = U*n("up",0)*n("dn",0) + U*n("up",1)*n("dn",1)
 H = H + 0.5*h*(n("up",0) - n("dn",0)) + 0.5*h*(n("up",1) - n("dn",1))
 
@@ -45,8 +50,10 @@ S = Solver(beta=beta, gf_struct=[["dn",2], ["up",2]], n_tau=n_tau, n_iw=n_iw)
 
 # Set hybridization function
 delta_w = GfImFreq(beta=beta, target_shape=(2,2))
-delta_w << (V**2)*(inverse(iOmega_n - epsilon) + inverse(iOmega_n + epsilon))
-for bn, g in S.G0_iw: g << inverse(iOmega_n - np.matrix([[-mu,t],[t,-mu]]) - delta_w)
+delta_w << inverse(iOmega_n - epsilon) + inverse(iOmega_n + epsilon)
+delta_w.from_L_G_R(V, delta_w, V)
+
+S.G0_iw << inverse(iOmega_n + mu - delta_w)
 
 # Solve!
 S.solve(h_int=H, **p)
@@ -55,7 +62,9 @@ if mpi.is_master_node():
     # Measure expectation values
     dm = S.density_matrix
     static_observables = {"N1_up" : n("up",0), "N1_dn" : n("dn",0),
-                          "N2_up" : n("up",1), "N2_dn" : n("dn",1)}
+                          "N2_up" : n("up",1), "N2_dn" : n("dn",1),
+                          "N12_up": c_dag("up",0) * c("up",1),
+                          "N12_dn": c_dag("dn",0) * c("dn",1)}
     with HDFArchive('measure_static_time_invariance.out.h5','w') as ar:
         for name,op in static_observables.items():
             ave = trace_rho_op(dm,op,S.h_loc_diagonalization)

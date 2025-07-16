44ed11654a563d1df5f87c3e8debd37cbc37588e
Thomas Hahn <thomas.hahn01@gmail.com>
2025-07-16 11:23:23 -0400

    Update slater.py test and reference results

--- test/python/slater.py
+++ test/python/slater.py
@@ -8,59 +8,64 @@ from triqs_cthyb import *
 from triqs.gf import *
 from triqs.utility.comparison_tests import *
 
-beta = 100.0
-# H_loc parameters
-L = 2 # angular momentum
-U = 5.0
-J = 0.1
-F0 = U
-F2 = J*(14.0/(1.0 + 0.63))
-F4 = F2*0.63
-half_bandwidth = 1.0
-mu = 32.5  # 3 electrons in 5 bands
+def test_slater(use_bound_as_threshold, h5_base_name):
+    beta = 100.0
+    # H_loc parameters
+    L = 2 # angular momentum
+    U = 5.0
+    J = 0.1
+    F0 = U
+    F2 = J*(14.0/(1.0 + 0.63))
+    F4 = F2*0.63
+    half_bandwidth = 1.0
+    mu = 32.5  # 3 electrons in 5 bands
 
-spin_names = ("up","down")
-cubic_names = [str(i) for i in range(2*L+1)]
-U_mat = U_matrix_slater(L, radial_integrals=[F0,F2,F4], basis="cubic")
+    spin_names = ("up","down")
+    cubic_names = [str(i) for i in range(2*L+1)]
+    U_mat = U_matrix_slater(L, radial_integrals=[F0,F2,F4], basis="cubic")
 
-# Parameters
-p = {}
-p["max_time"] = -1
-p["random_name"] = ""
-p["random_seed"] = 123 * mpi.rank + 567
-p["length_cycle"] = 50
-p["n_warmup_cycles"] = 50
-p["n_cycles"] = 5000
-p["measure_G_l"] = True
-p["move_double"] = False
+    # Parameters
+    p = {}
+    p["max_time"] = -1
+    p["random_name"] = ""
+    p["random_seed"] = 123 * mpi.rank + 567
+    p["length_cycle"] = 50
+    p["n_warmup_cycles"] = 50
+    p["n_cycles"] = 5000
+    p["measure_G_l"] = True
+    p["move_double"] = False
+    p["use_bound_as_threshold"] = use_bound_as_threshold
 
-# Block structure of GF
-gf_struct = set_operator_structure(spin_names,len(cubic_names),False)
+    # Block structure of GF
+    gf_struct = set_operator_structure(spin_names,len(cubic_names),False)
 
-# Local Hamiltonian
-H = h_int_slater(spin_names,cubic_names,U_mat,False)
+    # Local Hamiltonian
+    H = h_int_slater(spin_names,cubic_names,U_mat,False)
 
-# Construct the solver
-S = Solver(beta=beta, gf_struct=gf_struct, n_iw=1025, n_tau=100000, n_l=50)
+    # Construct the solver
+    S = Solver(beta=beta, gf_struct=gf_struct, n_iw=1025, n_tau=100000, n_l=50)
 
-# Set hybridization function
-delta_w = GfImFreq(beta=beta, target_shape=[1,1])
-delta_w << (half_bandwidth/2.0)**2 * SemiCircular(half_bandwidth)
-for name, g0 in S.G0_iw:
-    g0 << inverse(iOmega_n + mu - delta_w)
+    # Set hybridization function
+    delta_w = GfImFreq(indices = [0], beta=beta)
+    delta_w << (half_bandwidth/2.0)**2 * SemiCircular(half_bandwidth)
+    for name, g0 in S.G0_iw:
+        g0 << inverse(iOmega_n + mu - delta_w)
 
 
-S.solve(h_int=H, **p)
+    S.solve(h_int=H, **p)
 
-if mpi.is_master_node():
-    with HDFArchive("slater.out.h5",'w') as Results:
-        Results["G_tau"] = S.G_tau
-        Results["G_leg"] = S.G_l
+    if mpi.is_master_node():
+        with HDFArchive(f"{h5_base_name}.out.h5",'w') as Results:
+            Results["G_tau"] = S.G_tau
+            Results["G_leg"] = S.G_l
 
-if mpi.is_master_node():
-    with HDFArchive("slater.ref.h5",'r') as Results:
-        assert_block_gfs_are_close(Results["G_tau"], S.G_tau, precision=5e-6)
-        assert_block_gfs_are_close(Results["G_leg"], S.G_l)
+    if mpi.is_master_node():
+        with HDFArchive(f"{h5_base_name}.ref.h5",'r') as Results:
+            assert_block_gfs_are_close(Results["G_tau"], S.G_tau, precision=5e-6)
+            assert_block_gfs_are_close(Results["G_leg"], S.G_l)
 
-from triqs.utility.h5diff import h5diff
-h5diff("slater.out.h5","slater.ref.h5", precision=5e-6)
+    from triqs.utility.h5diff import h5diff
+    h5diff(f"{h5_base_name}.out.h5", f"{h5_base_name}.ref.h5", precision=5e-6)
+
+test_slater(True, 'slater.use_threshold')
+test_slater(False, 'slater.no_threshold')
